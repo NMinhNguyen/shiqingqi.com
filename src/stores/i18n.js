30fd4a06240bbe9aaa159edad06b06415769e3f5
Qingqi Shi <qingqishi@live.com>
2019-04-30 23:14:40 +0100

    feat(store): add an i18n store module

---
+++ src/stores/i18n.js
@@ -0,0 +1,70 @@
+import Vue from "vue";
+
+export const state = {
+  locale: "en",
+  /** @type {{[locale:string]: {[key:string]: string}}} */
+  translations: {}
+};
+
+export const mutations = {
+  /**
+   * @param {typeof state} state State object
+   * @param {string} locale Locale code
+   */
+  setLocale(state, locale) {
+    state.locale = locale;
+  },
+
+  /**
+   * @param {typeof state} state State object.
+   * @param {{lang: string, translations: object}} payload New translations.
+   */
+  setTranslations(state, { lang, translations }) {
+    Vue.set(state.translations, lang, {
+      ...state.translations[lang],
+      ...translations
+    });
+  }
+};
+
+export const actions = {
+  /**
+   * @param {import('vuex').Store<typeof state>} context
+   * @param {string} lang Language code.
+   */
+  async fetchTranslations({ commit }, lang) {
+    /** @type {{default: object}} */
+    const module = await import(`@/constants/translations/${lang}.json`);
+    commit("setTranslations", { lang, translations: module.default });
+  }
+};
+
+export const getters = {
+  /**
+   * @param {typeof state} state
+   * @returns {(key: string, params: {[key:string]: string}) => string}
+   */
+  getText: state => (key, params) => {
+    const translations = state.translations[state.locale];
+
+    // Return empty string if translation doesn't exist
+    if (!translations || !translations[key]) return "";
+
+    // Return translation if no params provided
+    if (!params) return translations[key];
+
+    // Replace the params
+    let text = translations[key];
+    for (let name in params) {
+      text = text.replace(`{{${name}}}`, params[name]);
+    }
+    return text;
+  }
+};
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters
+};
---
+++ tests/unit/stores/i18n.spec.js
@@ -0,0 +1,78 @@
+// @ts-nocheck
+import { mutations, actions, getters } from "@/stores/i18n";
+
+jest.mock("@/constants/translations/zh", () => ({
+  test: "bob"
+}));
+
+describe("i18n", () => {
+  describe("mutations", () => {
+    it("setLocale", () => {
+      const state = { locale: "test" };
+      mutations.setLocale(state, "en");
+      expect(state.locale).toEqual("en");
+    });
+
+    it("setTranslations zh", () => {
+      const state = { locale: "zh", translations: {} };
+      mutations.setTranslations(state, {
+        lang: "zh",
+        translations: { test: "test" }
+      });
+      expect(state.translations).toEqual({ zh: { test: "test" } });
+    });
+
+    it("setTranslations en", () => {
+      const state = { locale: "en", translations: {} };
+      mutations.setTranslations(state, {
+        lang: "en",
+        translations: { test: "test" }
+      });
+      expect(state.translations).toEqual({ en: { test: "test" } });
+    });
+  });
+
+  describe("actions", () => {
+    it("fetchTranslations", async () => {
+      const commit = jest.fn();
+      await actions.fetchTranslations({ commit }, "zh");
+      expect(commit).toHaveBeenCalledWith("setTranslations", {
+        lang: "zh",
+        translations: { test: "bob" }
+      });
+    });
+  });
+
+  describe("getters", () => {
+    describe("getText", () => {
+      const state = {
+        locale: "zh",
+        translations: {
+          zh: {
+            test: "test",
+            hasParam: "hello {{name}}",
+            multiple: "{{1}}{{2}}"
+          }
+        }
+      };
+      const get = getters.getText(state);
+
+      it("returns empty string if key doesn't exist", () => {
+        expect(get("blah")).toEqual("");
+      });
+
+      it("returns translation without param", () => {
+        expect(get("test")).toEqual("test");
+        expect(get("hasParam")).toEqual("hello {{name}}");
+      });
+
+      it("returns with param replaced", () => {
+        expect(get("hasParam", { name: "qi" })).toEqual("hello qi");
+      });
+
+      it("inserts multiple params", () => {
+        expect(get("multiple", { 1: "a", 2: "b" })).toEqual("ab");
+      });
+    });
+  });
+});
